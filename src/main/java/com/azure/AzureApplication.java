1c3187e01807920e978152a3473a999edf2b7493
hbansal944 <98749203+hbansal944@users.noreply.github.com>
2024-01-22 13:52:03 +0530

    Update AzureApplication.java

--- src/main/java/com/azure/AzureApplication.java
+++ src/main/java/com/azure/AzureApplication.java
@@ -4,7 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class AzureApplication {
+
+	@GetMapping("/message")
+	public String message() {
+		return "congrats";
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureApplication.class, args);
